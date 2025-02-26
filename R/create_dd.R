c8c95d0bcc5b1b19b0734a1b6198dfe90f958244
Castory Munishi <castoryofficial@gmail.com>
2025-02-26 20:01:42 +0300

    Create data dictionary

--- R/create_dd.R
+++ R/create_dd.R
@@ -9,7 +9,8 @@
 #' @return A named character vector where the old names are the names and the new names are the values.
 #'
 #' @examples
-#' dd <- data.frame(old_name = c("Patient ID", "Patient's Age", "Patient Sex"), new_name = c("ID", "age", "sex"))
+#' dd <- data.frame(old_name = c("Patient ID", "Patient's Age", "Patient Sex"),
+#'                 new_name = c("ID", "age", "sex"))
 #' create_dd(dd, "old_name", "new_name")
 #' # Named vector: c(Patient ID = "ID", B = "age", C = "sex")
 #' @export
