6db86c947f97cbc48b4ff03823632a6c2f8549af
Castory Munishi <castoryofficial@gmail.com>
2025-02-26 11:39:54 +0300

    Add sanity checks to the function

--- R/create_dd.R
+++ R/create_dd.R
@@ -1,5 +1,8 @@
 create_dd <-
 function(.data, old_name="old_name", new_name="new_name"){
+  if(!is.character(.data$old_name) | !is.character(.data$new_name)){
+    stop("Values to create a data dictionary must be character vectors.")
+  }
   with(.data, {
     names(new_name) <- old_name
     new_name
